d3287f875747e40ebbac4e035bb665f147610426
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2020-12-09 14:56:28 -0500

    added args parser comments

--- args.py
+++ args.py
@@ -1,6 +1,11 @@
 import argparse
 
 def make_parser():
+    """Makes a argument parser for some camera arguments
+
+    Returns:
+        argparse.ArgumentParser: The parser with the correct arguments
+    """
     parser = argparse.ArgumentParser(description="Process the cmd arguments to the script")
     parser.add_argument("-c", "--cam", type=int, default=0, help="The camera id to use")
     parser.add_argument("-v", "--video", type=str, default=None, help="The video to use as the source for the capture")
