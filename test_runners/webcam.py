67b709ad85b8ee33a962d666563b6b23cfa7da3c
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-01-13 12:22:23 -0500

    moved package code into folder

--- test_runners/webcam.py
+++ test_runners/webcam.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-PACKAGE_PARENT = '..'
+PACKAGE_PARENT = '../cv2utils'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
