194023f1031063e71962350b7b5ec90d75da57db
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-01-13 11:21:04 -0500

    added root dir to runtime python path

--- test_runners/main.py
+++ test_runners/main.py
@@ -1,4 +1,12 @@
 """Main script"""
+
+import sys
+import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
 from camera import make_camera_with_args
 
 camera, args = make_camera_with_args()
